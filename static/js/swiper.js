119353baa69e2556ed0de46a6705e454506afad8
miamar <mia.markanovic16@gmail.com>
2023-10-04 13:25:09 +0200

    case studies - swiper

---
+++ static/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 500000
+      }
+  });
