1967a017842ac92467cfc7cf40eaa5df64f14c49
miamar <mia.markanovic16@gmail.com>
2023-10-05 14:25:37 +0200

    header js - nav-link

--- static/js/header.js
+++ static/js/header.js
@@ -8,4 +8,15 @@ $(window).scroll(function() {
     }
 });
 
-
+// Add an event listener to each nav-link to close the navbar when clicked
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        const navbarNav = document.querySelector('.navbar-collapse');
+        if (navbarToggler && navbarNav) {
+            navbarToggler.classList.add('collapsed');
+            navbarNav.classList.remove('show');
+        }
+    });
+});
